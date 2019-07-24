39ff1f160d433efb856b4cc05840df56f1f3401d
DTangrini <52496846+DTangrini@users.noreply.github.com>
2019-07-24 16:39:10 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { BookInfo } from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] = 
+[
+  {bookname: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', genre: 'Fiction', published: 1999, picturepath: 'assets/images/HP3.PNG'},
+  {bookname: 'Death Note: Black Edition', author: 'Tsugumi Ohba & Takeshi Obata', genre: 'Manga', published: 2012, picturepath: 'assets/images/DN.jpg'},
+  {bookname: 'The Silk Roads', author: 'Peter Frankopan', genre: 'History', published: 2015, picturepath: 'assets/images/SR.jpg'},
+  {bookname: 'The Alchemist', author: 'Paulo Coelho', genre: 'Fiction', published: 1993, picturepath: 'assets/images/TA.jpg'}
+]
